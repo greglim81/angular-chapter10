44a5f9c682171ddfeb1171c7134d828235107770
Greg Lim <limjunqu@gmail.com>
2020-04-02 16:07:48 +0800

    Update github.component.ts

--- github.component.ts
+++ github.component.ts
@@ -1,53 +1,35 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import { FormControl } from '@angular/forms'; 
 import { GitHubService } from './github.service'; 
+import { filter,debounceTime,distinctUntilChanged } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',  
-    styles: [`
-      .img {
-          position: relative;
-          float: left;
-          width:  100px;
-          height: 100px;
-          background-position: 50% 50%;
-          background-repeat:   no-repeat;
-          background-size:     cover;
-      }            
-    `],  
-  template: `
-      <input class="form-control" type="search" [formControl]="searchControl">  
-      <div *ngIf="isLoading">
-        <i class="fa fa-spinner fa-spin fa-3x"></i>
+  selector: 'github',
+  template: `  
+  <input class="form-control" type="search" [formControl]="searchControl">
+  <h3>GitHub User Results</h3>    
+  <div *ngIf="isLoading">
+    <i class="fa fa-spinner fa-spin fa-3x"></i>
+  </div>    
+  <div *ngFor="let user of users" class="media">
+    <div class="media">
+      <a href="{{ user.html_url }}">
+        <img src="{{ user.avatar_url }}" class="mr-4" alt="..." width="64" height="64">
+      </a>
+      <div class="media-body">
+        <h5 class="mt-0">{{ user.login }}</h5>
+        Score: {{ user.score }}
       </div>
-      <h3>GitHub User Results</h3>    
-      <div *ngIf="isLoading">
-        <i class="fa fa-spinner fa-spin fa-3x"></i>
-      </div>    
-      <div *ngFor="let user of users" class="media">
-          <div class="media-left">              
-              <a [routerLink]="['user',user.login, user.score]">
-              <img class="media-object img" 
-                src="{{ user.avatar_url }}" alt="...">
-              </a>
-          </div>
-          <div class="media-body">
-              <h4 class="media-heading">{{ user.login }}</h4>
-              Score: {{ user.score }}               
-          </div>
-      </div>                                                                       
-    `,
+    </div>
+  </div>  
+  `,
   providers: [GitHubService]
-    
 })
 export class GitHubComponent {
-  isLoading = true;
-  users = [];
-  searchControl = new FormControl();
+  searchControl = new FormControl();  
+  isLoading = false;
+
+  users = [];  
 
   constructor(private _githubService: GitHubService){
   }
@@ -62,6 +44,6 @@ export class GitHubComponent {
                   this.isLoading = false;        
                   this.users = data.items;                    
           });                                  
-    });                    
-  } 
-}
+    });          
+  }
+}  
