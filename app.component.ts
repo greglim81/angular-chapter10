35f9638d1edf94af251dade9137a0da8878eef91
Greg Lim <limjunqu@gmail.com>
2020-04-03 11:25:23 +0800

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,28 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',  
-  template: `
+  selector: 'app-root',
+  template: `  
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" routerLink="">Home<span class="sr-only">(current)</span></a>
-        </li>        
-        <li class="nav-item">
-          <a class="nav-link" routerLink="GitHub">GitHub</a>        
-        </li>
-      </ul>
-    </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" routerLink="">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" routerLink="GitHub">GitHub</a>
+          </li>           
+        </ul>    
+      </div>
     </nav>
-    <router-outlet></router-outlet>                                                                   
-    `,
+    <router-outlet></router-outlet>                                                                      
+  `,
   providers: []
-    
 })
-export class AppComponent {
+export class AppComponent {   
 
   constructor(){
   }
-}
+}  
