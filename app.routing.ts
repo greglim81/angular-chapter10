d8f7a9185cc6074cae274580cb186e1e1ea78913
Greg Lim <limjunqu@gmail.com>
2020-04-03 11:27:19 +0800

    Update app.routing.ts

--- app.routing.ts
+++ app.routing.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './notfound.component';
-import { GitHubComponent } from './github.component';
 import { GitHubUserComponent } from './githubuser.component';
+import { GitHubComponent } from './github.component';
 
 import { LoginComponent } from './login.component';
 import { AuthGuard } from './auth-guard.service';
 
 import { PreventUnsavedChangesGuard } from './prevent-unsaved-changes-guard.service';
 
+
 export const routing = RouterModule.forRoot([
   {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'GitHub', component: GitHubComponent, canActivate: [AuthGuard]},
   {path: 'GitHub/user/:login/:score', component: GitHubUserComponent, canActivate: [AuthGuard]},
-  {path: 'login', component: LoginComponent, canDeactivate: [PreventUnsavedChangesGuard]},
+  {path: 'login', component: LoginComponent},
   {path: '**', component: NotFoundComponent}
 ]);
